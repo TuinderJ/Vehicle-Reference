289a0a335d358572c2504912dc8abc305c3deca3
TuinderJ <joshua.tuinder@gmail.com>
2022-11-18 23:07:36 -0700

    added associations for tables

--- models/index.js
+++ models/index.js
@@ -5,6 +5,27 @@ const Label = require('../models/Label');
 const Value = require('../models/Value');
 const ValueLabelVehicle = require('../models/ValueLabelVehicle');
 
+Category.hasMany(Label, { foreignKey: 'category_id' });
+Label.belongsTo(Category, { foreignKey: 'category_id' });
+
+Label.belongsToMany(Value, {
+  through: ValueLabelVehicle,
+  foreignKey: 'label_id',
+});
+Value.belongsToMany(Label, {
+  through: ValueLabelVehicle,
+  foreignKey: 'value_id',
+});
+
+Vehicle.belongsToMany(Value, {
+  through: ValueLabelVehicle,
+  foreignKey: 'vehicle_id',
+});
+Value.belongsToMany(Vehicle, {
+  through: ValueLabelVehicle,
+  foreignKey: 'value_id',
+});
+
 module.exports = {
   User,
   Category,
