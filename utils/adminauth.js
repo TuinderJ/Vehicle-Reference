79d26700f5e097a1209b64c2ce6525cc4a763e1b
CoderGirl <angi.adema@gmail.com>
2022-11-22 10:04:22 -0700

    corrected the adminauth.js file

--- utils/adminauth.js
+++ utils/adminauth.js
@@ -1,10 +1,18 @@
-const adminAuth = (req, res, next) => {
-    if (req.session.logged_in && adminAuth) {
-      next();
-    } else {
-      res.redirect('/login');
-    }
-  };
+function adminAuth(role) {
+  return (req, res, next) => {
+    res.status(401)
+    return res.send('No permissions');
+  }
+  next();
+}
+
+// const adminAuth = (req, res, next) => {
+//     if (req.session.logged_in && adminAuth) {
+//       next();
+//     } else {
+//       res.redirect('/login');
+//     }
+//   };
 
   
   module.exports = adminAuth;
--- utils/auth.js
+++ utils/auth.js
@@ -1,20 +1,12 @@
-function adminAuth(role) {
-  return (req, res, next) => {
-    res.status(401)
-    return res.send('No permissions');
-  }
-  next();
-}
-
-// const withAuth = (req, res, next) => {
+const withAuth = (req, res, next) => {
   
-//   if (!req.session.logged_in) {
-//     res.redirect('/login');
-//   } else {
-//     next();
-//   }
-// };
+  if (!req.session.logged_in) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+};
 
-module.exports = adminAuth;
+module.exports = withAuth;
 
 
