13602cd1d795fad731ca645fc37644e74278375d
CoderGirl <angi.adema@gmail.com>
2022-11-23 22:03:53 -0700

    updated vehicleRoutes.js

--- controllers/api/vehicleRoutes.js
+++ controllers/api/vehicleRoutes.js
@@ -1,62 +1,117 @@
 const router = require('express').Router();
-const { NewVehicle } = require('../../models');
-// const withAuth = require();
+const { Vehicle, Category, Label, Value } = require('../../models');
+const withAuth = require('../../utils/auth');
+const { Op } = require('sequelize');
 
-// //Search for a single vehicle, all can view. MAY NEED TO UPDATE HANDLEBARS TITLE IN res.render.
 router.get('/', async (req, res) => {
-    try {
-        const vehicles =  await NewVehicle.findAll()
-        // const singleVehicle = await Vehicle.findByPk(req.params.id, {
-        //     where: {},
-        //     include: {},
-
-        // });
-        // if (singleVehicle) {
-        //     const vehicle = singleVehicle.get({ plain: true });
-
-        //     res.render('single-vehicle', { vehicle });
-        // } else {
-        //     if (!singleVehicle) {
-        //         res.status(404).json({ message: 'No vehicle found with that id!' });
-        //         return;
-        //     }
-        // }
-        res.status(200).json(vehicles);
-    } catch (err) {
-        res.status(500).json(err);
+  try {
+    const { unitNumber, customerUnitNumber, vin, last8 } = req.query;
+    if (!unitNumber && !customerUnitNumber && !vin && !last8) {
+      return res
+        .status(400)
+        .json({ message: 'Provide something to search for.' });
     }
+    let searchTerm = null;
+    if (unitNumber) {
+      searchTerm = { unitNumber };
+    } else if (customerUnitNumber) {
+      searchTerm = { customerUnitNumber };
+    } else if (vin) {
+      searchTerm = { vin };
+    } else if (last8) {
+      searchTerm = { vin: { [Op.endsWith]: last8 } };
+    }
+
+    const data = await Vehicle.findAll({
+      where: { ...searchTerm },
+      // where: { vin: { [Op.endsWith]: last8 } },
+      include: {
+        model: Category,
+        attributes: {
+          exclude: ['vehicleCategory'],
+        },
+        include: {
+          model: Label,
+          attributes: {
+            exclude: ['categoryId'],
+          },
+        },
+      },
+    });
+
+    const vehicleData = data.map((vehicle) => vehicle.get({ plain: true }));
+
+    console.log(vehicleData);
+    if (!vehicleData[0]) {
+      return res
+        .status(404)
+        .json({ message: 'The vehicle searched for is not found.' });
+    }
+
+    vehicleData.forEach(async (vehicle) => {
+      const id = vehicle.id;
+      console.log(vehicle);
+
+      const valueData = await Vehicle.findAll({
+        where: { id },
+        attributes: {
+          exclude: [Vehicle],
+        },
+        include: {
+          model: Value,
+          attributes: {
+            exclude: ['value_vehicle'],
+          },
+        },
+      });
+     
+      vehicle.categories.forEach((category) => {
+        category.labels.forEach((label) => {
+          valueData[0].values.forEach(({ id, value, labelId }) => {
+            if (label.id === labelId) {
+              label.push(values: [{ id, value }]);
+            }
+          });
+        });
+      });
+    });
+
+
+    res.json(vehicleData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 // //Create a new vehicle, only admin and logged in users.
-// router.post('/', withAuth, async (req, res) => {
-//     try {
-//         // const addVehicle = await Vehicle.create(req.body);
-//         // res.status(200).json(addVehicle);
-//     } catch (err) {
-//         res.status(400).json(err);
-//     }
-// });
+router.post('/', withAuth, async (req, res) => {
+  try {
+    // const addVehicle = await Vehicle.create(req.body);
+    // res.status(200).json(addVehicle);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
-// //Update vehicle, only admin and logged in users.
-// router.put('/:id', withAuth, async (req, res) => {
-//     try {
-//         // const updateVehicle = await Vehicle.update(
-//         //     req.body,
-//         //     {
-//         //         where: {
-
-//         //         },
-//         //     });
-
-//         //     if (!updateVehicle) {
-//         //         res.status(404).json({ message: 'No vehicle found with that id!' });
-//         //         return;
-//         //     }
-//         //     res.status(200).json(updateVehicle);
-//     } catch (err) {
-//         res.status(500).json(err);
-//     }
-// });
+//Update vehicle, only admin and logged in users.
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    // const updateVehicle = await Vehicle.update(
+    //     req.body,
+    //     {
+    //         where: {
+    //         },
+    //     });
+    //     if (!updateVehicle) {
+    //         res.status(404).json({ message: 'No vehicle found with that id!' });
+    //         return;
+    //     }
+    //     res.status(200).json(updateVehicle);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // //Delete vehicle, ONLY ADMIN.
 // router.delete('/:id', withAuth, async (req, res) => {
