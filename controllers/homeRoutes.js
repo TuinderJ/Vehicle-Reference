c4190e2730bfe74e1f7973f0f5cd10e664cebf44
CoderGirl <angi.adema@gmail.com>
2022-11-29 12:28:39 -0700

    updated userRoutes.js, login.js and homeRoutes.js

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,16 +8,27 @@ router.get('/', async (req, res) => {
     const { unitNumber, customerUnitNumber, vin, last8 } = req.query;
     if (unitNumber || customerUnitNumber || vin || last8) {
       // AXIOS REQUEST
-      const domain = false || 'http://localhost:3001'
-      const data = await axios.get(`${domain}/api/vehicle`, { params: req.query});
+      const domain = false || 'http://localhost:3001';
+      const data = await axios.get(`${domain}/api/vehicle`, { params: req.query });
       console.log(data);
       res.render('homepage', data.data[0]);
-
     } else {
       res.render('homepage', {
         logged_in: req.session.logged_in,
       });
     }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  try {
+    if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+    res.render('login');
   } catch (err) {
     res.status(500).json(err);
   }
