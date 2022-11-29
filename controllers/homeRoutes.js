5939976c22d60c5fddde15f6139f9cf981c5b319
TuinderJ <joshua.tuinder@gmail.com>
2022-11-28 22:02:01 -0700

    small update

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,54 +1,39 @@
 const router = require('express').Router();
 const { Vehicle, Category, Label, Value } = require('../models');
-const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    const data = await Vehicle.findAll({
-      where: { unitNumber: '139406' },
-      include: {
-        model: Category,
-        attributes: {
-          exclude: ['vehicleCategory'],
-        },
-        include: {
-          model: Label,
-          attributes: {
-            exclude: ['categoryId'],
-          },
-        },
-      },
-    });
+    // const data = await Vehicle.findAll({
+    //   where: { unitNumber: '139406' },
+    //   include: {
+    //     model: Category,
+    //     attributes: { exclude: ['vehicleCategory'] },
+    //     include: {
+    //       model: Label,
+    //       attributes: { exclude: ['categoryId'] },
+    //     },
+    //   },
+    // });
 
-    const vehicleData = data.map((vehicle) => vehicle.get({ plain: true }));
+    // const vehicleData = data.map((vehicle) => vehicle.get({ plain: true }));
 
-    const id = data[0].id;
+    // const id = data[0].id;
 
-    const valueData = await Vehicle.findAll({
-      where: { id },
-      attributes: {
-        exclude: [Vehicle],
-      },
-      include: {
-        model: Value,
-        attributes: {
-          exclude: ['value_vehicle'],
-        },
-      },
-    });
+    // const valueData = await Vehicle.findAll({
+    //   where: { id },
+    //   attributes: { exclude: [Vehicle] },
+    //   include: { model: Value },
+    // });
 
-    vehicleData[0].categories.forEach((category) => {
-      category.labels.forEach((label) => {
-        valueData[0].values.forEach(({ id, value, labelId }) => {
-          if (label.id === labelId) label.values = [{ id, value }];
-        });
-      });
-    });
+    // vehicleData[0].categories.forEach((category) => {
+    //   category.labels.forEach((label) => {
+    //     valueData[0].values.forEach(({ id, value, labelId }) => {
+    //       if (label.id === labelId) label.values = [{ id, value }];
+    //     });
+    //   });
+    // });
 
-    // res.json(data);
-    res.render('homepage', {
-      logged_in: req.session.logged_in,
-    });
+    res.render('homepage', { logged_in: req.session.logged_in });
   } catch (err) {
     res.status(500).json(err);
   }
