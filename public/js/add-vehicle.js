8fa9d440b90d660381f6396cc1555f72bbf05544
Sean Grant <104331199+seanmgrant@users.noreply.github.com>
2022-11-29 17:39:42 -0700

    added js

--- public/js/add-vehicle.js
+++ public/js/add-vehicle.js
@@ -1,3 +1,19 @@
-const newFormHandler = async (event) => {
-    
+const addCardHandler = async (event) => {
+    event.preventDefault();
+
+    const unitNumber  = document.querySelector('input[name="unit-num"]').value;
+    const customerUnitNumber = document.querySelector('input[name="customer-unit-num"]').value;
+    const vin = document.querySelector('input[name="vin-input"]').value;
+
+    await fetch("/api/vehicle", {
+        method: 'POST',
+        body: JSON.stringify({
+            unitNumber,
+            customerUnitNumber,
+            vin
+        })
+    });
+    document.location.replace('/');
 }
+
+document.querySelector('new-card-form').addEventListener('submit', addCardHandler);
